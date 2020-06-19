3c9559ac744ebfcc64e9fe0dc5fcdd87f480c30e
ThatcherDev <thatcher.clough@gmail.com>
2020-06-19 12:44:19 -0400

    Possibly fixed an issue with flickering on windows

--- src/main/java/com/github/thatcherdev/asciianimator/AsciiAnimator.java
+++ src/main/java/com/github/thatcherdev/asciianimator/AsciiAnimator.java
@@ -7,21 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AsciiAnimator {
 
 	private static String file;
-	private static boolean loop;
+	private static boolean loop = false;
+	private static boolean clear = true;
 	private static int wait;
-	final private static String help = "AsciiAnimator: A stop motion ASCII art animator (1.0.1)\n\nUsage:\n\tjava -jar asciianimator.jar [-h] [-v] [-f FILE -l BOOLEAN -fps INTEGER]\n\n"
-			+ "Arguments:\n\t-h, --help\t\t\tDisplay this message.\n\t-v, --version\t\t\tDisplay current version.\n\t-f, --file\t\t\tSpecify file to use for animation. (See README.md for syntax)\n"
-			+ "\t-l, --loop\t\t\tSpecify if the animation should loop. (Set to false by default)\n\t-fps, --frames-per-second\tSpecify FPS for animation. (Must be an integer greater than 0)\n\n"
-			+ "Note: When running, CTRL + C can be used to terminate.";
-	final private static Thread clear = new Thread() {
-		public void run() {
-			System.out.print(Ansi.ansi().eraseScreen().cursor(0, 0).reset());
-		}
-	};
+	final private static String help = "AsciiAnimator: A stop motion ASCII art animator (1.1.0)\n\nUsage:\n\tjava -jar asciianimator.jar [-h] [-v] [-f FILE -l BOOLEAN " +
+			"-c BOOLEAN -fps INTEGER]\n\nArguments:\n\t-h, --help\t\t\tDisplay this message.\n\t-v, --version\t\t\tDisplay current version.\n\t-f, --file\t\t\tSpecify file " +
+			"to use for animation. (See README.md for syntax)\n\t-l, --loop\t\t\tSpecify if the animation should loop. (Set to false by default)\n\t-c, --clear" +
+			"\t\t\tSpecify if screen should be cleared after every frame. (Should be set to true if the frames have different lengths)\n\t-fps, --frames-per-second\tSpecify" +
+			" FPS for animation. (Must be an integer greater than 0)\n\nNote: When running, CTRL + C can be used to terminate.";
+	final private static Thread clearScreen = new Thread(() -> System.out.print(Ansi.ansi().eraseScreen().cursor(0, 0).reset()));
 
 	/**
-	 * Starts AsciiAnimator based on command line arguments {@link args}.
-	 * 
+	 * Starts AsciiAnimator based on command line arguments {@code args}.
+	 *
 	 * @param args command line arguments
 	 */
 	public static void main(String[] args) {
@@ -38,17 +36,19 @@ public class AsciiAnimator {
 					file = args[++k];
 				else if (args[k].equals("-l") || args[k].equals("--loop"))
 					loop = Boolean.parseBoolean(args[++k]);
+				else if (args[k].equals("-c") || args[k].equals("--clear"))
+					clear = Boolean.parseBoolean(args[++k]);
 				else if (args[k].equals("-fps") || args[k].equals("--frames-per-second"))
 					wait = 1000 / Integer.parseInt(args[++k]);
 			if (file == null || wait == 0)
 				throw new Exception();
 			try {
-				Runtime.getRuntime().addShutdownHook(clear);
-				Animator animator = new Animator(file, loop, wait);
-				clear.run();
+				Runtime.getRuntime().addShutdownHook(clearScreen);
+				Animator animator = new Animator(file, loop, clear, wait);
+				clearScreen.start();
 				animator.start();
 			} catch (Exception e) {
-				Runtime.getRuntime().removeShutdownHook(clear);
+				Runtime.getRuntime().removeShutdownHook(clearScreen);
 				e.printStackTrace();
 			}
 		} catch (Exception e) {
